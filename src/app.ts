439629ba175ab3652bec8ecdaa1cb1f68f32d1b4
nityam2002 <84077950+nityam2002@users.noreply.github.com>
2023-12-30 04:49:20 +0530

    refactor the structure

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,6 @@
 // app.ts
 import { expressMiddleware } from '@apollo/server/express4';
 import express from 'express';
-import userRoutes from './routes/userRoutes';
-import contentRoutes from './routes/contentRoutes';
 import errorMiddleware from './middlewares/errorMiddleware';
 import cors from 'cors';
 import createApolloGraphqlServer from './graphql';
@@ -17,13 +15,10 @@ const PORT = 3000;
 async function init() {
   app.use(express.json(), cors<cors.CorsRequest>());
 
+  app.use(errorMiddleware);
   app.use('/graphql', expressMiddleware(await createApolloGraphqlServer()));
-  // Routes
-  app.use('/users', userRoutes);
-  app.use('/content', contentRoutes);
 
   // Error handling middleware
-  app.use(errorMiddleware);
 
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
